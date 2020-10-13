f63d6e3ec99adddfd8d8934432253a95fec2151a
nikhil <nikhilcares22@gmail.com>
2020-10-13 08:31:23 +0530

    script change and port change

--- index.js
+++ index.js
@@ -46,6 +46,7 @@ app.use('/', indexRoutes);
 app.use('/campgrounds', campgroundRoutes);
 app.use('/campgrounds/:id/comments', commentRoutes);
 
-app.listen(port, () => {
-    console.log(`Server running on port ${port}`)
-})
+// app.listen(port, () => {
+//     console.log(`Server running on port ${port}`)
+// });
+app.listen(process.env.PORT, process.env.IP);
